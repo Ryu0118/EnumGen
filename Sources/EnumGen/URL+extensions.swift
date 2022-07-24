80d36a65c41783cd51e9e289622d3a33136646ba
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-07-24 21:53:52 +0900

    added main code

---
+++ Sources/EnumGen/URL+extensions.swift
@@ -0,0 +1,14 @@
+//
+//  URL+extensions.swift
+//  WebComposeTest
+//
+//  Created by Ryu on 2022/07/24.
+//
+
+import Foundation
+
+extension URL {
+    var isDirectory: Bool {
+       (try? resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory == true
+    }
+}
