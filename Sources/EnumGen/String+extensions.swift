f5633114db6e4b307e85c843f1e8887a3a1c84d6
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-07-24 23:09:38 +0900

    fix: remove unnecessary code

--- Sources/EnumGen/String+extensions.swift
+++ Sources/EnumGen/String+extensions.swift
@@ -9,10 +9,6 @@ import Foundation
 
 extension String {
 
-    mutating func newLine() {
-        self += "\n"
-    }
-    
     mutating func addLine(_ string: String, newLineCount: Int = 1, indentCount: Int = 0) {
         self += String(repeating: "\n", count: newLineCount) + String(repeating: "    ", count: indentCount) + string
     }
